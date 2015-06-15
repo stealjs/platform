3b8e0e8cb80e4241ea5492caa3ca31362817147c
Matthew Phillips <matthew@matthewphillips.info>
2015-06-15 08:41:18 -0400

    Fix cordova detection

--- platform.js
+++ platform.js
@@ -1,19 +1,29 @@
 define(["@loader"], function(loader){
 	var global = loader.global;
 
+
 	// Local copy of Node require
 	var nodeRequire = loader._nodeRequire;
 
 	var isNode = typeof process === "object" &&
-			{}.toString.call(process) === "[object process]"
+			{}.toString.call(process) === "[object process]";
+
+	var ua = navigator.userAgent;
+
+	var isiOS = /iPhone OS/.test(ua);
+	var isAndroid = /Android/.test(ua);
 
 	var platform = {
 
 		// Node.js
 		isNode: isNode,
 
+		isiOS: isiOS,
+
+		isAndroid: isAndroid,
+
 		// Cordova
-		isCordova: !!global.cordova,
+		isCordova: (isiOS || isAndroid) && document.location.protocol === "file:",
 
 		// NW.js
 		isNW: isNode && (function(){
